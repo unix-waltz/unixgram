1a29897f6a4595391784803574fac829042a5e5d
unix-waltz <riosraskaa@gmail.com>
2024-03-06 08:27:48 +0700

    fix muddleware bugs

--- resources/views/Client/index.blade.php
+++ resources/views/Client/index.blade.php
@@ -54,7 +54,7 @@
                     @endphp
                 
                     @foreach ($d->postLikes as $like)
-                    
+                    @auth
                         @if ($like->userid == auth()->user()->id)
                             @php
                                 $userLiked = true;
@@ -70,14 +70,17 @@
                             </form>
                             @break
                         @endif
+                        @endauth
                     @endforeach
                 
                     @unless($userLiked)
                         <form action="/like" method="post">
                             @csrf
                             @method('POST')
+                            @auth
                             <input type="hidden" name="userid" value="{{ auth()->user()->id }}">
                             <input type="hidden" name="postid" value="{{ $d->id }}">
+                            @endauth
                             <button type="submit" title="Like post" class="flex items-center justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current">
                                     <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
@@ -119,6 +122,7 @@
                                         @endphp
 
                                         @foreach ($d->postSaves as $like)
+                                        @auth
                                             @if ($like->userid == auth()->user()->id)
                                                 @php
                                                     $userLiked = true;
@@ -137,14 +141,17 @@
                                                 </form>
                                             @break
                                         @endif
+                                        @endauth
                                     @endforeach
 
                                     @unless ($userLiked)
                                         <form action="/save" method="post">
                                             @csrf
                                             @method('POST')
+                                            @auth
                                             <input type="hidden" name="userid" value="{{ auth()->user()->id }}">
                                             <input type="hidden" name="postid" value="{{ $d->id }}">
+                                            @endauth
                                             <button type="submit" title="Like post"
                                                 class="flex items-center justify-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
@@ -217,7 +224,9 @@
                   <form action="/comment" method="post">
                     @csrf
                     @method('POST')
+                    @auth
                     <input type="hidden" name="userid" value="{{Auth()->user()->id}}">
+                    @endauth
                     <input type="hidden" name="postid" value="{{$d->id}}">
                     <input type="text" name="comment" id="commentInput_{{$d->id}}" placeholder="Add a comment..." class="w-full py-0.5 bg-transparent border-none rounded text-sm pl-0 text-coolGray-100" style="cursor: auto;">
                   </form>
